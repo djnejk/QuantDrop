2226a73e1b878ea611dc4f34335d57e00b30c8de
DjNejk - notebook <djnejk@djdevs.eu>
2026-05-26 22:30:26 +0200

    Scaffold QuantDrop Fabric mod

---
+++ src/client/java/cz/jirik/quantdrop/client/QuantDropClient.java
@@ -0,0 +1,9 @@
+package cz.jirik.quantdrop.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class QuantDropClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/cz/jirik/quantdrop/QuantDropMod.java
@@ -0,0 +1,15 @@
+package cz.jirik.quantdrop;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class QuantDropMod implements ModInitializer {
+	public static final String MOD_ID = "quantdrop";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("QuantDrop initialized");
+	}
+}
